8cb992268663cc11c35d46bc06f2d086a3d45130
Christophe Weibel <ratfox@google.com>
2026-02-01 14:15:33 +0100

    wishes

--- Assets/Scripts/Playerproximity.cs
+++ Assets/Scripts/Playerproximity.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerProximity : MonoBehaviour
 {
     public float exchangeRadius = 1f;    // Distance where exchange is possible
+    public float visibilityRadius = 2f;    // Distance where visibility starts
     public string playerTag = "Player";
     GameObject playerObj;
     void Start()
@@ -14,12 +15,8 @@ public class PlayerProximity : MonoBehaviour
     private void FixedUpdate()
     {
         float distance = Vector2.Distance(transform.position, playerObj.transform.position);
-        if (distance < exchangeRadius)
-        {
-            transform.Find("EnemyBubble").GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
-        } else
-        {
-            transform.Find("EnemyBubble").GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
-        }
+        float alpha = Mathf.Clamp01(visibilityRadius - distance) / (visibilityRadius - exchangeRadius);
+        transform.Find("EnemyBubble").GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+        transform.Find("EnemyBubble/EnemyWish").GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
     }
 }
